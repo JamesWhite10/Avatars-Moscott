d061fe47e56a32fdeffc8b525e04971a5b1c4d44
Цветов Сергей <cvetov.sergej@avsconsulting.ru>
2022-11-07 22:15:35 +0300

    NBE-148: Add damping for rotate mascott

--- src/modules/editor/scene/cameraControls/CameraControls.ts
+++ src/modules/editor/scene/cameraControls/CameraControls.ts
@@ -10,35 +10,35 @@ export interface ObjectControlsOptions {
 export class CameraControls {
   public velocityMousePosition: Vector2 = new Vector2();
 
-  public amplitude = 4;
-
   public mousePosition: THREE.Vector2 = new THREE.Vector2();
 
   public prevMousePosition: Vector2 = new Vector2();
 
   public diffMousePosition: THREE.Vector2 = new THREE.Vector2();
 
-  public speedRotate = 5;
+  public speedRotate: number = 5;
 
-  public speedParallax = 0.05;
+  public speedParallax: number = 0.05;
 
-  public enableDamping = true;
-
-  public dampingFactor = 0.1;
-
-  public enableParallax = true;
+  public enableParallax: boolean = true;
 
   public rendererWidth: number;
 
   public rendererHeight: number;
 
-  public object?: THREE.Object3D;
+  public object?: THREE.Object3D = undefined;
 
   public threeCamera: THREE.PerspectiveCamera;
 
   public lookVector = new THREE.Vector3(1.6, 1, 0);
 
-  public isMoving = true;
+  public isMovingCamera: boolean = true;
+
+  public targetRotationX: number = 0;
+
+  public clientXClickDown: number = 0;
+
+  public targetRotationOnMouseDownX: number = 0;
 
   constructor(options: ObjectControlsOptions) {
     this.threeCamera = options.threeCamera;
@@ -46,13 +46,12 @@ export class CameraControls {
     this.rendererWidth = options.width;
   }
 
-  public setObject(object: THREE.Object3D) {
-    this.object = object;
+  public onUpdate(): void {
+    this.rotateObject();
   }
 
-  public update(): void {
-    this.updateObject();
-    this.updateCameraParallax();
+  public setObject(object: THREE.Object3D) {
+    this.object = object;
   }
 
   public updateCameraParallax() {
@@ -69,18 +68,9 @@ export class CameraControls {
     this.prevMousePosition.copy(this.mousePosition);
   }
 
-  public updateObject() {
-    if (!this.object) return;
-    if (this.enableDamping) {
-      this.object.rotation.y += this.amplitude * this.diffMousePosition.x * (1 - this.dampingFactor);
-    } else {
-      this.object.rotation.y -= this.amplitude * this.diffMousePosition.x;
-    }
-
-    if (this.enableDamping) {
-      this.diffMousePosition.multiplyScalar(1 - this.dampingFactor);
-    } else {
-      this.diffMousePosition.set(0, 0);
+  public rotateObject(): void {
+    if (this.object) {
+      this.object.rotation.y += (this.targetRotationX - this.object.rotation.y) * 0.01;
     }
   }
 }
